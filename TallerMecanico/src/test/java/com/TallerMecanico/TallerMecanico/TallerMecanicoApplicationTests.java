7c689885abe51eeb7edc282ce0818437d40ea140
henadalGH <hrnnnadal@gmail.com>
2025-04-23 17:20:38 -0300

    hola

---
+++ TallerMecanico/src/test/java/com/TallerMecanico/TallerMecanico/TallerMecanicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TallerMecanico.TallerMecanico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TallerMecanicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
